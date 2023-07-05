f629e1c22eabd2d504d4501a82ca6dd2578194ee
oocim <116318345+oocim@users.noreply.github.com>
2023-07-05 22:57:31 +0800

    Add new features

--- main.cpp
+++ main.cpp
@@ -3,28 +3,36 @@
 class EmployeeList {
 private:
     struct Employee {
+        int id;
         std::string name;
         std::string bday;
+        std::string gender;
         int age;
+        std::string civil;
         std::string position;
+        std::string department;
+        std::string status;
+        std::string phone;
         std::string email;
         std::string address;
         std::string bank;
         Employee* next;
 
-        Employee(const std::string& name, const std::string& bday, int age, const std::string& position, const std::string& email, const std::string& address, const std::string& bank)
-            : name(name), bday(bday), age(age), position(position), email(email), address(address), bank(bank), next(nullptr) {}
+        Employee(int id, const std::string& name, const std::string& bday, std::string& gender, int age, std::string& civil, const std::string& position, std::string& status, std::string& department, std::string& phone, const std::string& email, const std::string& address, const std::string& bank)
+    : id(id), name(name), bday(bday), gender(gender), age(age), civil(civil), position(position), department(department), status(status), phone(phone), email(email), address(address), bank(bank), next(nullptr) {}
+
     };
 
     Employee* head;
     Employee* tail;
+    int nextId;
 
 public:
-    EmployeeList() : head(nullptr), tail(nullptr) {}
+    EmployeeList() : head(nullptr), tail(nullptr), nextId(1) {}
 
-    // Add Employee and Details
-    void addEmployee(const std::string& name, const std::string& bday, int age, const std::string& position, const std::string& email, const std::string& address, const std::string& bank) {
-        Employee* newEmployee = new Employee(name, bday, age, position, email, address, bank);
+    void addEmployee(const std::string& name, const std::string& bday, std::string& gender, int age, std::string& civil, const std::string& position, std::string& status, std::string& department, std::string& phone, const std::string& email, const std::string& address, const std::string& bank) {
+        Employee* newEmployee = new Employee(nextId, name, bday, gender, age, civil, position, status, department, phone, email, address, bank);
+        nextId++;
 
         if (head == nullptr) {
             head = newEmployee;
@@ -35,28 +43,172 @@ public:
         }
     }
 
+
+    void deleteEmployee(int id) {
+        if (head == nullptr) {
+            std::cout << "Employee not found!" << std::endl;
+            return;
+        }
+
+        if (head->id == id) {
+            Employee* temp = head;
+            head = head->next;
+            delete temp;
+            std::cout << "Employee deleted successfully." << std::endl;
+            return;
+        }
+
+        Employee* current = head;
+        while (current->next != nullptr) {
+            if (current->next->id == id) {
+                Employee* temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+                std::cout << "Employee deleted successfully." << std::endl;
+                return;
+            }
+            current = current->next;
+        }
+
+        std::cout << "Employee not found!" << std::endl;
+    }
+
     void displayEmployees() const {
         Employee* current = head;
         while (current != nullptr) {
-            std::cout << "Name: " << current->name << std::endl;
+            std::cout << current->id << ") Name: " << current->name << std::endl;
             current = current->next;
         }
     }
 
-    void searchEmployee(const std::string& name) const {
+    void searchEmployee(int id) const {
+            Employee* current = head;
+            bool found = false;
+
+            while (current != nullptr) {
+                if (current->id == id) {
+                    std::cout << "Employee Information:" << std::endl;
+                    std::cout << "ID: " << current->id << std::endl;
+                    std::cout << "Name: " << current->name << std::endl;
+                    std::cout << "Birthday: " << current->bday << std::endl;
+                    std::cout << "Gender: " << current->gender << std::endl;
+                    std::cout << "Age: " << current->age << std::endl;
+                    std::cout << "Civil Status: " << current->civil << std::endl;
+                    std::cout << "Position: " << current->position << std::endl;
+                    std::cout << "Department: " << current->department << std::endl;
+                    std::cout << "Employment Status: " << current->status << std::endl;
+                    std::cout << "Phone Number: " << current->phone << std::endl;
+                    std::cout << "Email Address: " << current->email << std::endl;
+                    std::cout << "Address: " << current->address << std::endl;
+                    std::cout << "Bank Information: " << current->bank << std::endl;
+
+                    found = true;
+                    break;
+                }
+                current = current->next;
+            }
+
+            if (!found) {
+                std::cout << "Employee not found!" << std::endl;
+            }
+        }
+
+        void editEmployee(int& id) {
+
         Employee* current = head;
         bool found = false;
 
         while (current != nullptr) {
-            if (current->name == name) {
-                std::cout << "Employee Information:" << std::endl;
-                std::cout << "Name: " << current->name << std::endl;
-                std::cout << "Birthday: " << current->bday << std::endl;
-                std::cout << "Age: " << current->age << std::endl;
-                std::cout << "Position: " << current->position << std::endl;
-                std::cout << "Email Address: " << current->email << std::endl;
-                std::cout << "Address: " << current->address << std::endl;
-                std::cout << "Bank Information: " << current->bank << std::endl;
+            if (current->id == id) {
+                std::cout << "Employee Found!" << std::endl;
+                std::cout << "Enter new employee details (leave blank to retain existing information):" << std::endl;
+
+                std::string name;
+                std::cout << "Name: ";
+                std::cin.ignore();
+                std::getline(std::cin, name);
+                if (!name.empty()) {
+                    current->name = name;
+                }
+
+                std::string bday;
+                std::cout << "Birthday: ";
+                std::getline(std::cin, bday);
+                if (!bday.empty()) {
+                    current->bday = bday;
+                }
+
+                std::string gender;
+                std::cout << "Gender: ";
+                std::getline(std::cin, gender);
+                if (!gender.empty()) {
+                    current->gender = gender;
+                }
+
+                std::string ageStr;
+                std::cout << "Age: ";
+                std::getline(std::cin, ageStr);
+                if (!ageStr.empty()) {
+                    int age = std::stoi(ageStr);
+                    current->age = age;
+                }
+
+                std::string civil;
+                std::cout << "Civil Status: ";
+                std::getline(std::cin, civil);
+                if (!civil.empty()) {
+                    current->civil = civil;
+                }
+
+                std::string position;
+                std::cout << "Position: ";
+                std::getline(std::cin, position);
+                if (!position.empty()) {
+                    current->position = position;
+                }
+
+                std::string department;
+                std::cout << "Department: ";
+                std::getline(std::cin, department);
+                if (!department.empty()) {
+                    current->department = department;
+                }
+
+                std::string status;
+                std::cout << "Employment Status: ";
+                std::getline(std::cin, status);
+                if (!status.empty()) {
+                    current->status = status;
+                }
+
+                std::string phone;
+                std::cout << "Phone Number: ";
+                std::getline(std::cin, phone);
+                if (!phone.empty()) {
+                    current->phone = phone;
+                }
+
+                std::string email;
+                std::cout << "Email Address: ";
+                std::getline(std::cin, email);
+                if (!email.empty()) {
+                    current->email = email;
+                }
+
+                std::string address;
+                std::cout << "Address: ";
+                std::getline(std::cin, address);
+                if (!address.empty()) {
+                    current->address = address;
+                }
+
+                std::string bank;
+                std::cout << "Bank Account Number: ";
+                std::getline(std::cin, bank);
+                if (!bank.empty()) {
+                    current->bank = bank;
+                }
+
                 found = true;
                 break;
             }
@@ -67,6 +219,7 @@ public:
             std::cout << "Employee not found!" << std::endl;
         }
     }
+
 
     void manageEmployee(){
         system("cls");
@@ -84,43 +237,65 @@ public:
                 case 'A': {
                     system("cls");
                     std::cout << "Employee Information Form" << std::endl;
+                    std::cin.ignore();
                     std::string name;
                     std::string bday;
+                    std::string gender;
                     int age;
+                    std::string civil;
                     std::string position;
+                    std::string department;
+                    std::string status;
+                    std::string phone;
                     std::string email;
                     std::string address;
                     std::string bank;
 
                     std::cout << "Name: ";
-                    std::cin >> name; 
+                    std::getline(std::cin, name);
                     std::cout << "Birthday: ";
-                    std::cin >> bday; 
+                    std::getline(std::cin, bday);
+                    std::cout << "Gender: ";
+                    std::getline(std::cin, gender);
                     std::cout << "Age: ";
                     std::cin >> age;
+                    std::cin.ignore();
+                    std::cout << "Civil Status: ";
+                    std::getline(std::cin, civil);
                     std::cout << "Position: ";
-                    std::cin >> position;
+                    std::getline(std::cin, position);
+                    std::cout << "Department: ";
+                    std::getline(std::cin, department);
+                    std::cout << "Employment Status: ";
+                    std::getline(std::cin, status);
+                    std::cout << "Phone Number: ";
+                    std::getline(std::cin, phone);
                     std::cout << "Email Address: ";
-                    std::cin >> email;
+                    std::getline(std::cin, email);
                     std::cout << "Address: ";
-                    std::cin >> address;
+                    std::getline(std::cin, address);
                     std::cout << "Bank Information: ";
-                    std::cin >> bank; 
+                    std::getline(std::cin, bank);
 
-                    addEmployee(name, bday, age, position, email, address, bank);
+                    addEmployee(name, bday, gender, age, civil, position, department, status, phone, email, address, bank);
                     system("pause");
                     system("cls");
                     break;
                 }
-
                 case 'B':
-                    // TODO: Edit employee function
+                    int id;
+                    std::cout << "Enter Employee ID" << std::endl;
+                    std::cin >> id;
+                    editEmployee(id);
                     system("pause");
                     system("cls");
                     break;
 
                 case 'C':
-                    // TODO: Delete employee function
+                    int search;
+                    std::cout << "Enter Employee ID" << std::endl;
+                    std::cin >> search;
+                    deleteEmployee(search);
                     system("pause");
                     system("cls");
                     break;
@@ -138,6 +313,7 @@ public:
 int main(){
     char choice;
     EmployeeList employeeList;
+    
     do{
         system("cls");
         std::cout << "Employee Database" << std::endl << std::endl;
@@ -160,7 +336,7 @@ int main(){
                 std::cin >> c;
                 if(toupper(c) == 'A'){
                     std::cout << "Enter Employee" << std::endl;
-                    std::string search;
+                    int search;
                     std::cin >> search;
                     employeeList.searchEmployee(search);
                 }
@@ -170,11 +346,9 @@ int main(){
                 system("cls");
                 break;
             case 'B':
-                std::cout << "Managing employees..." << std::endl;
                 employeeList.manageEmployee();
                 break;
             case 'C':
-                std::cout << "Processing employee payroll..." << std::endl;
                 // TODO: Implement logic for processing payroll
                 system("pause");
                 system("cls");
